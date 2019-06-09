949a50ba1b63f89bd24401e797a8b461bb5cfa79
Glitch (sid7-fcc-project-urlshortener-microservice) <none>
2019-06-09 22:15:27 +0000

    🐲🏗 Checkpoint ./app.js:3898527/2809

--- app.js
+++ app.js
@@ -9,13 +9,14 @@ function sanitizeURL(url) {
   }
   return url;
 }
-function isValidURL(url, cb) {
+function checkURL(url, cb) {
   try {
     url = new URL(url);
   } catch(err) {
     return false;
   }
   dns.lookup(url.hostname, function(err) {
+    console.log(arguments);
     cb(err ? false : true);
   });
 }
@@ -28,40 +29,41 @@ exports.genShortUrl = function(req, res) {
   
   const url = sanitizeURL(req.body.url);
   
-  if(!isValidURL(url)) {
-    res.json({ error: "Invalid URL" });
-    return;
-  }
-  
-  
-  ShortUrl.findOne({ url: url }, function(err, sUrl) {
-    if(err) {
-      console.log({ err: err, at: "check-for-duplicate" });
-      res.json({ error: err, at: "check-for-duplicate" });
+  checkURL(url, function(isValid) {
+    if(!isValid) {
+      res.json({ error: "Invalid URL" });
       return;
     }
-    if(sUrl) {
-      res.json({ original_url: url, short_url: sUrl.slug });
-    } else {
-      console.log("gen new short-url");
-      Counter.findOneAndUpdate({}, { $inc: { count: 1 } }, { new: true, upsert: true }, function(err, doc) {
-        const slug = doc.count;
-        if(err || !slug) {
-          console.log({ err: err, at: "failed-to-gen-slug", doc: doc });
-          res.json({ error: err, at: "gen-slug" });
-          return;
-        }
-        const newShortUrl = new ShortUrl({ url: url, slug: slug });
-        newShortUrl.save(function(err) {
-          if(err) {
-            res.json({ error: err, at: "saving-new-short-url" });
-          } else {
-            res.json({ original_url: url, short_url: slug  });
+    
+    ShortUrl.findOne({ url: url }, function(err, sUrl) {
+      if(err) {
+        console.log({ err: err, at: "check-for-duplicate" });
+        res.json({ error: err, at: "check-for-duplicate" });
+        return;
+      }
+      if(sUrl) {
+        res.json({ original_url: url, short_url: sUrl.slug });
+      } else {
+        console.log("gen new short-url");
+        Counter.findOneAndUpdate({}, { $inc: { count: 1 } }, { new: true, upsert: true }, function(err, doc) {
+          const slug = doc.count;
+          if(err || !slug) {
+            console.log({ err: err, at: "failed-to-gen-slug", doc: doc });
+            res.json({ error: err, at: "gen-slug" });
+            return;
           }
-        })
-      });
-    }
-  })
+          const newShortUrl = new ShortUrl({ url: url, slug: slug });
+          newShortUrl.save(function(err) {
+            if(err) {
+              res.json({ error: err, at: "saving-new-short-url" });
+            } else {
+              res.json({ original_url: url, short_url: slug  });
+            }
+          });
+        });
+      }
+    });
+  });
 }
 
 exports.redirectToOriginalUrl = function(req, res) {
